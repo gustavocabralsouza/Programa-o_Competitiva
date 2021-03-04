0368425f7fb74da9b49b9989ab94dbdf962934e4
gustavocabralsouza <gustavocsouza65@gmail.com>
2021-03-04 17:38:10 -0300

    problem 1002 added

---
+++ Beginner_Problems/Problem_1002.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+  double raio, pi, area;
+  pi = 3.14159;
+  
+  scanf("%lf",&raio);
+  
+  area = pi * raio * raio;
+  
+  printf("A=%.4lf\n", area);
+  return 0;
+}
