ba91ace73bd91ca2abf5a5ea4f28b3e832a3a318
gustavocabralsouza <gustavocsouza65@gmail.com>
2021-03-04 18:21:49 -0300

    problem 1007 added

---
+++ Beginner_Problems/Problem_1007.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+  int A, B, C, D, diference;
+  
+  scanf("%d",&A);
+  scanf("%d",&B);
+  scanf("%d",&C);
+  scanf("%d",&D);
+  
+  diference = ((A*B)- (C*D));
+    
+  printf("DIFERENCA = %d\n",diference);
+  return 0;
+}
