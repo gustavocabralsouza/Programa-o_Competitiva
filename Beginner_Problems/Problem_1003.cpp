b1720db4115751bc6d38bc14caa2364a8d53c258
gustavocabralsouza <gustavocsouza65@gmail.com>
2021-03-04 17:43:58 -0300

    problem 1003 added

---
+++ Beginner_Problems/Problem_1003.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+  int A, B, SOMA;
+  
+  scanf("%d",&A);
+  scanf("%d",&B);
+  
+  SOMA = A + B;
+  
+  printf("SOMA = %d\n",SOMA);
+  return 0;
+}
