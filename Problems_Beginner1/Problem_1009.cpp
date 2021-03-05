492473e1389ba450cc53c77c72d0cc06f877f878
gustavocabralsouza <gustavocsouza65@gmail.com>
2021-03-05 11:20:03 -0300

    added problem 1009

---
+++ Problems_Beginner1/Problem_1009.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+  char name[61];
+  double salary_fix,sales_total, salary;
+  
+  scanf("%s",name);
+  scanf("%lf",&salary_fix);
+  scanf("%lf",&sales_total);
+  
+  salary = salary_fix + (0.15 * sales_total);
+  
+  printf("TOTAL = R$ %.2lf\n",salary);
+  
+  return 0;
+}
